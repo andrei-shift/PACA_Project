2f14f8704bd653a27f66a3e0ba07de3b5715ec1c
Camille Barneaud <camille.barneaud@shift-technology.com>
2020-02-20 19:35:35 +0100

    Awesome scorer v1

--- TemplateHashCode2020/Scorer.cs
+++ TemplateHashCode2020/Scorer.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TemplateHashCode2020
 {
@@ -6,9 +8,42 @@ namespace TemplateHashCode2020
     {
         public static int ComputeScore(SolutionInstance po, ProblemInstance problem)
         {
-            //var total = po.SignUpLibraryList.Sum(l => problem.Books[l].NumberOfSlices);
-            //return total > problem.MaximumSlices ? 0 : total;
-            return 0;
+            var bookIdsScanned = new HashSet<int>();
+            var effectiveLibraryTimes = new Dictionary<int, int>();
+
+            // Compute effective signup times
+            var currentTime = 0;
+            foreach (var library in po.SignUpLibraryList)
+            {
+                var timeToSignUp = library.TimeToSignUp;
+                var libraryId = library.Id;
+
+                currentTime += timeToSignUp;
+
+                var usefulTime = problem.NumberOfDays - currentTime;
+                if (usefulTime > 0)
+                {
+                    effectiveLibraryTimes.Add(libraryId, usefulTime);
+                }
+                else
+                {
+                    Console.WriteLine($"Library {libraryId} is useless as signed up too late.");
+                }
+            }
+
+            // Compute really shipped book
+            foreach (var library in po.SignUpLibraryList)
+            {
+                var libraryId = library.Id;
+                var effectiveLibraryCapacity = library.ShippingCapacity * effectiveLibraryTimes[libraryId];
+
+                foreach (var book in po.ShippingOrders[libraryId].Take(effectiveLibraryCapacity))
+                {
+                    bookIdsScanned.Add(book);
+                }
+            }
+
+            return bookIdsScanned.Sum(bookId => problem.Books[bookId].Value);
         }
     }
 }
