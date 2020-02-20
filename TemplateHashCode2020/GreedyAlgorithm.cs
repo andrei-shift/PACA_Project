da392d7c6f0be9a7d7b7161ec3c75ed6a214ea85
Pierre Lataillade <pierre.lataillade@shift-technology.com>
2020-02-20 20:00:28 +0100

    less stupid

--- TemplateHashCode2020/GreedyAlgorithm.cs
+++ TemplateHashCode2020/GreedyAlgorithm.cs
@@ -22,6 +22,7 @@ namespace TemplateHashCode2020
 			var time = 0;
 			HashSet<Library> doneLibraries = new HashSet<Library>();
 			HashSet<Library> availableLibraries = new HashSet<Library>(Problem.Libraries);
+			var doneBooks = new HashSet<int>();
 			List<Library> output = new List<Library>();
 			Dictionary<int, List<int>> shippingOrder = new Dictionary<int, List<int>>();
 			while (time < Problem.NumberOfDays)
@@ -31,11 +32,13 @@ namespace TemplateHashCode2020
 				doneLibraries.Add(selectedLib);
 				availableLibraries.Remove(selectedLib);
 				output.Add(selectedLib);
-				var doneBooks = new HashSet<int>(doneLibraries.SelectMany(lib => lib.BooksId));
 				shippingOrder[selectedLib.Id] =
 					selectedLib.BooksId.OrderByDescending(bookId => BookScore(bookId, doneBooks)).ToList();
 				time += selectedLib.TimeToSignUp;
-
+				foreach (var id in selectedLib.BooksId)
+				{
+					doneBooks.Add(id);
+				}
 				Console.WriteLine($"{time}  {Problem.NumberOfDays}");
 			}
 			solution.SignUpLibraryList = output;
