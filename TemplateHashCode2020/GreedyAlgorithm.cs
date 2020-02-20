4a111dc3f3a60cc0239100ffa2fb5d1caf3c834b
Camille Barneaud <camille.barneaud@shift-technology.com>
2020-02-20 19:59:12 +0100

    IsEmpty

--- TemplateHashCode2020/GreedyAlgorithm.cs
+++ TemplateHashCode2020/GreedyAlgorithm.cs
@@ -24,7 +24,7 @@ namespace TemplateHashCode2020
 			HashSet<Library> availableLibraries = new HashSet<Library>(Problem.Libraries);
 			List<Library> output = new List<Library>();
 			Dictionary<int, List<int>> shippingOrder = new Dictionary<int, List<int>>();
-			while (time < Problem.NumberOfDays)
+			while (time < Problem.NumberOfDays && availableLibraries.Count != 0)
 			{
 				var pOrderLibrary = availableLibraries.OrderByDescending(lib => LibraryScore(lib, time, doneLibraries));
 				var selectedLib = pOrderLibrary.First();
